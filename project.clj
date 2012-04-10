766cd3dc903dec7636b1fddd17f4698db9e0fc80
pgdad <pgdad@hotmail.com>
2012-04-09 20:05:01 -0400

    add logging

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
-(defproject clj-jetty-proxy "1.0.0"
+(defproject clj-jetty-proxy "1.0.1"
   :description "FIXME: write description"
   :dependencies
   [[org.clojure/clojure "1.3.0"]
-   [org.eclipse.jetty/jetty-servlet "8.0.4.v20111024"]
-   [org.eclipse.jetty/jetty-servlets "8.0.4.v20111024"]
-   [clj-zoo-watcher "1.0.1"]
-   [clj-tree-zipper "1.0.0"]]
+   [org.eclipse.jetty/jetty-servlet "8.1.2.v20120308"]
+   [org.eclipse.jetty/jetty-servlets "8.1.2.v20120308"]
+   [clj-zoo-watcher "1.0.3"]
+   [clj-tree-zipper "1.0.0"]
+   [log4j/log4j "1.2.16"]
+   [org.clojure/tools.logging "0.2.3"]]
   :repl-init clj-jetty-proxy.proxyservlet
   :main clj-jetty-proxy.proxyservlet
   :aot [clj-jetty-proxy.proxyservlet])
