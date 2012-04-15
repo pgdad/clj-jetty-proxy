0122492e4337f7aadfb3c5e40cc9d547a7a1a7f5
pgdad <pgdad@hotmail.com>
2012-04-15 16:13:33 -0400

    init

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject clj-jetty-proxy "1.0.1"
+(defproject clj-jetty-proxy "1.0.2"
   :description "FIXME: write description"
   :dependencies
   [[org.clojure/clojure "1.3.0"]
    [org.eclipse.jetty/jetty-servlet "8.1.2.v20120308"]
    [org.eclipse.jetty/jetty-servlets "8.1.2.v20120308"]
    [clj-zoo-watcher "1.0.3"]
+   [clj-zoo-service-tracker "1.0.0"]
    [clj-tree-zipper "1.0.0"]
    [log4j/log4j "1.2.16"]
    [org.clojure/tools.logging "0.2.3"]]
