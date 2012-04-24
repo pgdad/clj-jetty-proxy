452dc938a84e4129a36181f289b18a81bd42e059
pgdad <pgdad@hotmail.com>
2012-04-24 18:14:06 -0400

    new dependency versions

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-jetty-proxy "1.0.3"
+(defproject clj-jetty-proxy "1.0.4"
   :description "FIXME: write description"
   :dependencies
   [[org.clojure/clojure "1.3.0"]
    [org.eclipse.jetty/jetty-servlet "8.1.3.v20120416"]
    [org.eclipse.jetty/jetty-servlets "8.1.3.v20120416"]
-   [clj-zoo-watcher "1.0.5"]
-   [clj-zoo-service-tracker "1.0.1"]
+   [clj-zoo-watcher "1.0.6"]
+   [clj-zoo-service-tracker "1.0.2"]
    [clj-tree-zipper "1.0.0"]
    [log4j/log4j "1.2.16"]
    [org.clojure/tools.logging "0.2.3"]]
